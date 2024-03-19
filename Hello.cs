c291697705c77c342125cf61ee8e603ad7ef52ef
sanjayerram <sanjay.erram@rediffmail.com>
2024-03-19 12:10:30 +0530

    Add project files.

---
+++ Hello.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Hello
+    {
+        public static void Main()
+        {
+            Console.WriteLine(  "Hello This is GIT Demo");
+            Console.ReadLine();
+        }
+    }
+}
